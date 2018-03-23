a51a276930f9fd9e86382fdd8b235334f9507411
ablodge <ablodge@users.noreply.github.com>
2018-03-22 22:41:00 -0400

    added html tags for sub/sup
    added html tags for subscript and superscript

--- testproject/testproject/settings/codehilite.py
+++ testproject/testproject/settings/codehilite.py
@@ -5,6 +5,14 @@ from testproject.settings.local import *
 
 # Test codehilite with pygments
 
+MARKDOWN_HTML_WHITELIST = ['a', 'abbr', 'acronym', 'b', 'blockquote', 
+                           'code', 'em', 'i', 'li', 'ol', 'strong', 
+                           'ul', 'figure', 'figcaption', 'br', 'hr', 
+                           'p', 'div', 'img', 'pre', 'span', 'sub', 'sup', 
+                           'table', 'thead', 'tbody', 'th', 'tr', 'td', 
+                           'dl', 'dt', 'dd', 'h0', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7']
+
+
 WIKI_MARKDOWN_KWARGS = {
     'extensions': [
         'codehilite',
